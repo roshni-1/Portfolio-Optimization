3d00f826d751baf8c47d0818746c43f60ddce20e
Roshni Yadav <47268468+roshni-1@users.noreply.github.com>
2024-02-19 22:21:00 +0530

    Update data_fetching.py

--- data_fetching.py
+++ data_fetching.py
@@ -1,17 +1,22 @@
+#Importing libraries 
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas_datareader import data as web
+from datetime import datetime
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt import risk_models
+from pypfopt import expected_returns
 import yfinance as yf
 
-# Define the list of stocks you want to analyze
 stock_symbols = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'META','NVDA','ASML','TSLA','APLT','NNOX','RYZB','COIN','ALCE','HTOO','LIVE','SOFI','DBX','BLZE','INTR','GXAI','MGX','IRDM','ZCARW','NDAQ','SMCI']
 
-# Set the start and end dates for data retrieval
 start_date = '2010-01-01'
 end_date = datetime.today().strftime('%Y-%m-%d')
 
-# Fetch historical stock prices from Yahoo Finance
 stock_data = yf.download(stock_symbols, start=start_date, end=end_date)['Adj Close']
 
-# Calculate daily returns
 returns = stock_data.pct_change()
 
-# Display the first few rows of the returns dataframe
 print(returns.head())
